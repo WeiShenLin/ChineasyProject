6a951c2506b62b12836d27435e7113923622612b
WeiShenLin <m19911207@gmail.com>
2015-12-21 20:55:02 +0800

    抽牌完成
    整個程式都完成了
    只有還沒debug

--- mainPlay.cpp
+++ mainPlay.cpp
@@ -11,7 +11,7 @@ typedef struct {
     char times[2];
 }Letters;
 typedef struct {
-    int hands[3];
+    char hands[3][3];
     int time;
     int score;
 }Players;
@@ -40,6 +40,18 @@ Letters letter[53];
 Bags bag;
 Boards board;
 
+void drawone(Players player,int draw_place){
+	for(int i=0;i<100;i++){
+		if(strcmp(bag.letters[i],"No"))strcpy(player.hands[draw_place],bag.letters[i]);
+	}
+	bag.num++;
+}
+void draw(Players player){
+	for(int i=0;i<3;i++){
+		if(strcmp(player.hands[i],"No"))drawone(player,i);
+	}
+}
+
 void shufflebag(){
 	for(int i=0;i<=10000;i++){
         int n1=rand()%100;		//產生0～99的亂數
@@ -52,9 +64,9 @@ void shufflebag(){
     }
 }
 
-void putbag(int hand1,int hand2){
-	strcpy(bag.letters[bag.num-1],block[hand1].letter);
-	strcpy(bag.letters[bag.num-2],block[hand2].letter);
+void putbag(char hand1[3],char hand2[3]){
+	strcpy(bag.letters[bag.num-1],hand1);
+	strcpy(bag.letters[bag.num-2],hand2);
 	bag.num-=2;
 	shufflebag();
 	
@@ -63,28 +75,28 @@ int checklayer2(int row , int col){
 	if(!strcmp(board.layer1_letters[row][col],"No")||!strcmp(board.layer1_letters[row+1][col],"No")||!strcmp(board.layer1_letters[row][col+1],"No")||!strcmp(board.layer1_letters[row+1][col+1],"No"))return 0;
 	else return 1;
 }
-int checkhands(Players player,Blocks block[100]){
+int checkhands(Players player){
 	FILE *fp;
 	char W_temp[50][5];
 	int word_num=0;
 	char word[6][5];
-	strcpy(word[0],block[player.hands[0]].letter);
-	strcat(word[0],block[player.hands[1]].letter);
-	strcpy(word[1],block[player.hands[0]].letter);
-	strcat(word[1],block[player.hands[2]].letter);
-	strcpy(word[2],block[player.hands[1]].letter);
-	strcat(word[2],block[player.hands[0]].letter);
-	strcpy(word[3],block[player.hands[1]].letter);
-	strcat(word[3],block[player.hands[2]].letter);
-	strcpy(word[4],block[player.hands[2]].letter);
-	strcat(word[4],block[player.hands[0]].letter);
-	strcpy(word[5],block[player.hands[2]].letter);
-	strcat(word[5],block[player.hands[1]].letter);
+	strcpy(word[0],player.hands[0]);
+	strcat(word[0],player.hands[1]);
+	strcpy(word[1],player.hands[0]);
+	strcat(word[1],player.hands[2]);
+	strcpy(word[2],player.hands[1]);
+	strcat(word[2],player.hands[0]);
+	strcpy(word[3],player.hands[1]);
+	strcat(word[3],player.hands[2]);
+	strcpy(word[4],player.hands[2]);
+	strcat(word[4],player.hands[0]);
+	strcpy(word[5],player.hands[2]);
+	strcat(word[5],player.hands[1]);
 	player.time++;	
-	if( ( fp = fopen(".Words.csv", "r")) == NULL) 
+	if( ( fp = fopen("Words.csv", "r")) == NULL) 
     {
         puts("Cannot open the file");
-        system("PAUSE");
+        //system("PAUSE");
         
     }
     
@@ -92,53 +104,60 @@ int checkhands(Players player,Blocks block[100]){
     	fscanf(fp,"%s",W_temp[word_num]);
     	if(!strcmp(W_temp[word_num],word[0])){
     		putbag(player.hands[0],player.hands[1]);
-			player.hands[0]=101;
-    		player.hands[1]=101;
+			strcpy(player.hands[0],"No");
+    		strcpy(player.hands[1],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
     	else if(!strcmp(W_temp[word_num],word[1])){
     		putbag(player.hands[0],player.hands[2]);
-			player.hands[0]=101;
-    		player.hands[2]=101;
+			strcpy(player.hands[0],"No");
+    		strcpy(player.hands[2],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
 		else if(!strcmp(W_temp[word_num],word[2])){
     		putbag(player.hands[1],player.hands[0]);
-			player.hands[1]=101;
-    		player.hands[0]=101;
+			strcpy(player.hands[1],"No");
+    		strcpy(player.hands[0],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
 		else if(!strcmp(W_temp[word_num],word[3])){
     		putbag(player.hands[1],player.hands[2]);
-			player.hands[1]=101;
-    		player.hands[2]=101;
+			strcpy(player.hands[1],"No");
+    		strcpy(player.hands[2],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
 		else if(!strcmp(W_temp[word_num],word[4])){
     		putbag(player.hands[2],player.hands[0]);
-			player.hands[2]=101;
-    		player.hands[0]=101;
+			strcpy(player.hands[2],"No");
+    		strcpy(player.hands[0],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
 		else if(!strcmp(W_temp[word_num],word[5])){
     		putbag(player.hands[2],player.hands[1]);
-			player.hands[2]=101;
-    		player.hands[1]=101;
+			strcpy(player.hands[2],"No");
+    		strcpy(player.hands[1],"No");
     		player.score++;
-    		return 2;
+    		fclose(fp);
+			return 2;
     	}
 		else
 		    word_num++;
     }
+    fclose(fp);
 	return 0;
 }
 
-int checkword(int handword){
+int checkword(char handword[3]){
 	FILE *fp;
 	char W_temp[50][5];
 	int word_num=0;
@@ -146,14 +165,14 @@ int checkword(int handword){
 	int word_place=0;
 	for(int i=0;i<7;i++){
 		for(int j=0;j<7;j++){
-			strcpy(word[word_place],block[handword].letter);
+			strcpy(word[word_place],handword);
 			strcpy(word[word_place],board.layer1_letters[i][j]);
 			word_place++;
 		}
 	}
 	for(int i=0;i<6;i++){
 		for(int j=0;j<6;j++){
-			strcpy(word[word_place],block[handword].letter);
+			strcpy(word[word_place],handword);
 			strcpy(word[word_place],board.layer2_letters[i][j]);
 			word_place++;
 		}
@@ -161,22 +180,22 @@ int checkword(int handword){
 	for(int i=0;i<7;i++){
 		for(int j=0;j<7;j++){
 			strcpy(word[word_place],board.layer1_letters[i][j]);
-			strcpy(word[word_place],block[handword].letter);
+			strcpy(word[word_place],handword);
 			word_place++;
 		}
 	}
 	for(int i=0;i<6;i++){
 		for(int j=0;j<6;j++){
 			strcpy(word[word_place],board.layer2_letters[i][j]);
-			strcpy(word[word_place],block[handword].letter);
+			strcpy(word[word_place],handword);
 			word_place++;
 		}
 	}
 	
-	if( ( fp = fopen(".Words.csv", "r")) == NULL) 
+	if( ( fp = fopen("Words.csv", "r")) == NULL) 
     {
         puts("Cannot open the file");
-        system("PAUSE");
+        //system("PAUSE");
     }
     while(word_num!=50){
     	fscanf(fp,"%s",W_temp[word_num]);
@@ -187,10 +206,11 @@ int checkword(int handword){
     	}
 		word_num++;
     }
-    return 0;
+    fclose(fp);
+	return 0;
 }
 
-void randboard(int num){
+void randboard(char num[3]){
 	int board_row,board_col,boardlayer;
 		while(1){
 			board_row=rand()%7;
@@ -201,8 +221,8 @@ void randboard(int num){
 				if(!strcmp(board.layer2_letters[board_row][board_col],"No"))break;
 			}
 		}
-		if(boardlayer==1)strcpy(board.layer1_letters[board_row][board_col],block[num].letter);
-		else strcpy(board.layer2_letters[board_row][board_col],block[num].letter);
+		if(boardlayer==1)strcpy(board.layer1_letters[board_row][board_col],num);
+		else strcpy(board.layer2_letters[board_row][board_col],num);
 }
 
 int checkboard(Players player,int player_num){
@@ -210,20 +230,20 @@ int checkboard(Players player,int player_num){
 		for(int j=0;j<7;j++){
 			if(!strcmp(board.layer1_letters[i][j],"No")){
 				if(checkword(player.hands[0])){
-					strcpy(board.layer1_letters[i][j],block[player.hands[0]].letter);
-					player.hands[0]=101;
+					strcpy(board.layer1_letters[i][j],player.hands[0]);
+					strcpy(player.hands[0],"No");
 					player.score++;
 					return 1;
 				}
 				else if(checkword(player.hands[1])){
-					strcpy(board.layer1_letters[i][j],block[player.hands[1]].letter);
-					player.hands[0]=101;
+					strcpy(board.layer1_letters[i][j],player.hands[1]);
+					strcpy(player.hands[1],"No");
 					player.score++;
 					return 1;
 				}
 				else if(checkword(player.hands[2])){
-					strcpy(board.layer1_letters[i][j],block[player.hands[2]].letter);
-					player.hands[0]=101;
+					strcpy(board.layer1_letters[i][j],player.hands[2]);
+					strcpy(player.hands[2],"No");
 					player.score++;
 					return 1;
 				}
@@ -234,36 +254,39 @@ int checkboard(Players player,int player_num){
 		for(int j=0;j<6;j++){
 			if(!strcmp(board.layer2_letters[i][j],"No")&&checklayer2(i,j)){
 				if(checkword(player.hands[0])){
-					strcpy(board.layer2_letters[i][j],block[player.hands[0]].letter);
-					player.hands[0]=101;
+					strcpy(board.layer2_letters[i][j],player.hands[0]);
+					strcpy(player.hands[0],"No");
 					player.score++;
 					return 1;
 				}
 				else if(checkword(player.hands[1])){
-					strcpy(board.layer2_letters[i][j],block[player.hands[1]].letter);
-					player.hands[1]=101;
+					strcpy(board.layer2_letters[i][j],player.hands[1]);
+					strcpy(player.hands[1],"No");
 					player.score++;
 					return 1;
 				}
 				else if(checkword(player.hands[2])){
-					strcpy(board.layer2_letters[i][j],block[player.hands[2]].letter);
-					player.hands[2]=101;
+					strcpy(board.layer2_letters[i][j],player.hands[2]);
+					strcpy(player.hands[2],"No");
 					player.score++;
 					return 1;
 				}
 			}	
 		}
 	}
-	if(player.hands[0]!=101){	
+	if(strcmp(player.hands[0],"No")){	
  		randboard(player.hands[0]);
+ 		strcpy(player.hands[0],"No");
  		return 1;
  	}
- 	else if(player.hands[1]!=101){
+ 	else if(strcmp(player.hands[1],"No")){
  		randboard(player.hands[1]);
+ 		strcpy(player.hands[1],"No");
  		return 1;
  	}
 	else{
  		randboard(player.hands[2]);
+ 		strcpy(player.hands[2],"No");
  		return 1;
 	}
 }
@@ -281,7 +304,7 @@ int main(){
 	int block_num=0;
 	bag.num=0;
 	
-    if( ( fp = fopen( "Letters_Time.csv", "r")) == NULL) 
+    if( ( fp = fopen("Letters_Time.csv", "r")) == NULL) 
     {
         puts("Cannot open the file");
     }
@@ -298,7 +321,7 @@ int main(){
     
     count=0;
 	while(block_num<100){
-		if(strcmp(letter[count].times,"2")==0){
+		if(!strcmp(letter[count].times,"2")){
 			strcpy(block[block_num].letter,letter[count].letter);
 			strcpy(block[block_num+1].letter,letter[count].letter); 
 			block_num+=2;
@@ -310,17 +333,10 @@ int main(){
 		count++;
 	}
     
-    for(int i=0;i<=10000;i++){
-        int n1=rand()%100;		//產生0～99的亂數
-        int n2=rand()%100;
-        //將陣列資料進行交換（打散）
-		Blocks B_temp;
-		strcpy(B_temp.letter,block[n1].letter);
-		strcpy(block[n1].letter,block[n2].letter);
-		strcpy(block[n2].letter,B_temp.letter);
-    }
     //for(int i=0;i<100;i++)printf("%s\n",block[i].word);
     for(int i=0;i<100;i++)strcpy(bag.letters[i],block[i].letter);
+    shufflebag();
+    
     
     printf("玩家人數:");
 	scanf("%d",&player);
@@ -329,47 +345,54 @@ int main(){
     	int draw_num=0;
 		bag.num+=player*3;
     	for(int i=0;i<player*3;i++)strcpy(bag.letters[i],"No");
+    	
 		Players A,B,C,D;
-		A.hands[0]=0;
-		A.hands[1]=1;
-		A.hands[2]=2;
+		strcpy(A.hands[0],bag.letters[0]);
+		strcpy(A.hands[1],bag.letters[1]);
+		strcpy(A.hands[2],bag.letters[2]);
 		A.time=0;
 		A.score=0;
-		B.hands[0]=3;
-		B.hands[1]=4;
-		B.hands[2]=5;
+		strcpy(B.hands[0],bag.letters[3]);
+		strcpy(B.hands[1],bag.letters[4]);
+		strcpy(B.hands[2],bag.letters[5]);
 		B.time=0;
 		B.score=0;
-		C.hands[0]=6;
-		C.hands[1]=7;
-		C.hands[2]=8;
+		strcpy(C.hands[0],bag.letters[6]);
+		strcpy(C.hands[1],bag.letters[7]);
+		strcpy(C.hands[2],bag.letters[8]);
 		C.time=0;
 		C.score=0;
-		D.hands[0]=9;
-		D.hands[1]=10;
-		D.hands[2]=11;
+		strcpy(D.hands[0],bag.letters[9]);
+		strcpy(D.hands[1],bag.letters[10]);
+		strcpy(D.hands[2],bag.letters[11]);
 		D.time=0;
 		D.score=0;
 		int boardspace=7*7+6*6; 
 		int deck_num=0;
 		while(1){
-			draw_num+=checkhands(A,block);
-			draw_num+=checkboard(A,player);
-			//draw(A,draw_num);
-			draw_num=0;
-			draw_num+=checkhands(B,block);
-			draw_num+=checkboard(B,player);
-			//draw(B,draw_num);
-			draw_num=0;
-			draw_num+=checkhands(C,block);
-			draw_num+=checkboard(C,player);
-			//draw(C,draw_num);
-			draw_num=0;
-			draw_num+=checkhands(D,block);
-			draw_num+=checkboard(D,player);
-			//draw(D,draw_num);
-			draw_num=0;
-			if(deck_num==100||boardspace==0)break;
+			checkhands(A);
+		    checkboard(A,player);
+			draw(A);
+			boardspace--;
+			if(deck_num==100||boardspace<=0)break;
+			checkhands(B);
+			checkboard(B,player);
+			draw(B);
+			boardspace--;
+			if(deck_num==100||boardspace<=0)break;
+			checkhands(C);
+			checkboard(C,player);
+			draw(C);
+			boardspace--;
+			if(deck_num==100||boardspace<=0)break;
+			checkhands(D);
+			checkboard(D,player);
+			draw(D);
+			boardspace--;
+			if(bag.num>=100||boardspace<=0)break;
+			
+			
+			
 		}
 		system("PAUSE"); 
 		printf("%d %d %d %d",A.score,B.score,C.score,D.score);
